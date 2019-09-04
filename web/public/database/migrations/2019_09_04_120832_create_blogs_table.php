be4c0f63a265bfb6d06ace9405b651657683243c
blaze <kontaktAK@Yandex.ru>
2019-09-04 15:21:21 +0300

    adding blog model/migration/controller bash script to execute migrations from docker container

---
+++ web/public/database/migrations/2019_09_04_120832_create_blogs_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogsTable extends Migration {
+    public function up() {
+        Schema::create( 'blogs', function ( Blueprint $table ) {
+            $table->bigIncrements( 'id' );
+            $table->string( 'title' );
+            $table->text( 'content' );
+            $table->integer( 'user_id' );
+            $table->timestamps();
+        } );
+    }
+
+    public function down() {
+        Schema::dropIfExists( 'blogs' );
+    }
+}
